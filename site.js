7075dcfcc292f6966d84609761fd4bf39bcdb74e
andre <ac639@njit.edu>
2019-04-29 14:26:14 -0400

    Added variables and vue_app to the main container

--- site.js
+++ site.js
@@ -32,9 +32,12 @@ const vue_app = new Vue({
       },
       data: {
             // This holds your movies.json data.
-            movies: []
+            movies: [],
 
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Andre's Top 8 Movies",
+            owner: 'Andre',
+            github: 'https://github.com/ac639/is219-p3'
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
