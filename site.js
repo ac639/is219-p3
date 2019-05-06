83572ca717018078ed9fda91d707585faa5d6d89
andre <ac639@njit.edu>
2019-05-05 23:00:42 -0400

    Added v-for loop and makeTextDate function

--- site.js
+++ site.js
@@ -41,6 +41,21 @@ const vue_app = new Vue({
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            makeTextDate(dateArray) {
+                  var year = dateArray[0];
+                  var month = dateArray[1];
+                  var day = dateArray[2];
+
+                  monthString = month.toString();
+
+                  var dateFormat = '';
+                  dateFormat = dateFormat + monthString + ' ' + day + ', ' + year;
+
+                  return dateFormat;
+
+
+
+            }
       }
 })
 	
