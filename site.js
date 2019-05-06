0284c31abe8dfb6f9ebf4aebd6fe2201e40996f4
andre <ac639@njit.edu>
2019-05-05 23:13:16 -0400

    Changed movie data binding to reflect for loop, altered makeTextData to include month

--- site.js
+++ site.js
@@ -46,10 +46,34 @@ const vue_app = new Vue({
                   var month = dateArray[1];
                   var day = dateArray[2];
 
-                  monthString = month.toString();
+                  if (month == 1) {
+                        month = 'January';
+                  } else if (month == 2) {
+                        month = 'February';
+                  } else if (month == 3) {
+                        month = 'March';
+                  } else if (month == 4) {
+                        month = 'April';
+                  } else if (month == 5) {
+                        month = 'May';
+                  } else if (month == 6) {
+                        month = 'June';
+                  } else if (month == 7) {
+                        month = 'July';
+                  } else if (month == 8) {
+                        month = 'August';
+                  } else if (month == 9) {
+                        month = 'September';
+                  } else if (month == 10) {
+                        month = 'October';
+                  } else if (month == 11) {
+                        month = 'November';
+                  } else if (month == 12) {
+                        month = 'December';
+                  }
 
                   var dateFormat = '';
-                  dateFormat = dateFormat + monthString + ' ' + day + ', ' + year;
+                  dateFormat = dateFormat + month + ' ' + day + ', ' + year;
 
                   return dateFormat;
 
